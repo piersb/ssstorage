ebc6085a529518fc403be427aeb5cd0195a8a880
Piers Beckley <piers@fatpigeons.com>
2019-09-01 11:17:15 +0100

    Layout and routes

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ gem 'jbuilder', '~> 2.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+# Bootstrap
+gem 'bootstrap-sass', '>= 3.3.6'
+
 group :development, :test do
   # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
---
+++ example_user.rb
@@ -0,0 +1,20 @@
+class User
+  attr_accessor :first, :last, :email
+  def initialize(attributes={})
+    @first = attributes[:first]
+    @last = attributes[:last]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+     full_name + " <#{@email}>"
+  end
+
+  def full_name
+    "#{@first} #{@last}"
+  end
+
+  def alphabetical_name
+    "#{@last}, #{@first}"
+  end
+end
