0541202797e79cf69e5cd02f1686f3bc7fcbd896
Piers Beckley <piers@fatpigeons.com>
2019-12-02 17:48:21 +0000

    update name of production server so password resets and signups work again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Raise email delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://blooming-depths-81623.herokuapp.com/'
+  host = 'https://frozen-mountain-52554.herokuapp.com'
   config.action_mailer.default_url_options = {host: host}
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
